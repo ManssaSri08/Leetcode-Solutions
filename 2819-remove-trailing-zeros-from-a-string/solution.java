32d35a80540c0c00755135ffb6db6d58cd48ab23
ManssaSri08 <manssahari0827@gmail.com>
2025-09-21 05:37:20 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - 45.1 MB (99.84%)

---
+++ 2819-remove-trailing-zeros-from-a-string/solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String removeTrailingZeros(String num) {
+        int len=num.length()-1;
+        while(len>=0 && num.charAt(len)=='0')
+            len--;
+        return num.substring(0,len+1);
+    }
+}
