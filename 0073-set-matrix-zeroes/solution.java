9e2887b2bb096f1db6050fdfedb465166e40df1f
ManssaSri08 <manssahari0827@gmail.com>
2025-09-21 05:51:48 +0000

    [LeetCode Sync] Runtime - 1 ms (97.47%), Memory - 45.9 MB (99.96%)

---
+++ 0073-set-matrix-zeroes/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int row=matrix.length;
+        int col=matrix[0].length;
+        int[][] mat=new int[row][col];
+        for(int i=0;i<row;i++)
+            for(int j=0;j<col;j++)
+                mat[i][j]=matrix[i][j];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(mat[i][j]==0){
+                    for(int k=0;k<col;k++)
+                        matrix[i][k]=0;
+                    for(int k=0;k<row;k++)
+                        matrix[k][j]=0;
+                }   
+            }   
+        }
+    }
+}
