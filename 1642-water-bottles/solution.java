42fb72a211a5555c8099a68031feb3f4865e771a
ManssaSri08 <manssahari0827@gmail.com>
2025-10-01 10:07:28 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.6 MB (99.94%)

---
+++ 1642-water-bottles/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int count=0,empty=0;
+        while(numBottles>=1)
+        {
+            count+=numBottles;
+            empty+=numBottles;
+            numBottles=empty/numExchange;
+            empty=empty%numExchange;
+        }
+        return count;
+    }
+}
