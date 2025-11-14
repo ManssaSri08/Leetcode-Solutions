6f2dc9d5f5b4a640668867749b7f2839f2f1b6dc
ManssaSri08 <manssahari0827@gmail.com>
2025-11-14 07:22:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43 MB (45.70%)

---
+++ 0953-reverse-only-letters/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        char[] arr=s.toCharArray();
+        int left=0,right=arr.length-1;
+        while(left<right){
+            if(!Character.isLetter(arr[left]))
+                left++;
+            else if(!Character.isLetter(arr[right]))
+                right--;
+            else{
+                char ch=arr[left];
+                arr[left]=arr[right];
+                arr[right]=ch;
+                left++; right--;
+            }
+        }
+        return new String(arr);
+    }
+}
