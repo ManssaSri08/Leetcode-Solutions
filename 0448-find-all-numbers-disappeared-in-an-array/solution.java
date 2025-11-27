7daa54c92aea44b3583f9a26f73173af8d5c81ff
ManssaSri08 <manssahari0827@gmail.com>
2025-11-27 09:25:06 +0000

    [LeetCode Sync] Runtime - 3 ms (99.91%), Memory - 66.6 MB (97.84%)

---
+++ 0448-find-all-numbers-disappeared-in-an-array/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        boolean[] present=new boolean[nums.length+1];
+        for(int x:nums) present[x]=true;
+        List<Integer> result=new ArrayList<>();
+        for(int i=1;i<=nums.length;i++){
+            if(!present[i])
+                result.add(i);
+        }
+        return result;
+    }
+}
