cdd4fbb9d14e1778d846bff7cfcd78863c9f6826
ManssaSri08 <manssahari0827@gmail.com>
2025-12-04 10:00:00 +0000

    [LeetCode Sync] Runtime - 10 ms (87.64%), Memory - 47.3 MB (98.08%)

---
+++ 2317-count-collisions-on-a-road/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countCollisions(String directions) {
+        int n=directions.length();
+        int l=0,r=n-1,collisions=0;
+        while(l<n && directions.charAt(l)=='L'){
+            l++;
+        }
+        while(r>=0 && directions.charAt(r)=='R'){
+            r--;
+        }
+        for(int i=l;i<=r;i++){
+            if(directions.charAt(i)!='S'){
+                collisions++;
+            }
+        }
+        return collisions;
+    }
+}
