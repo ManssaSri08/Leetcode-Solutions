d767c60caa3f5de18d3f0d43c11778fbfea9c84c
ManssaSri08 <manssahari0827@gmail.com>
2025-12-20 07:12:47 +0000

    [LeetCode Sync] Runtime - 8 ms (68.99%), Memory - 47.2 MB (16.10%)

---
+++ 0981-delete-columns-to-make-sorted/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        int row=strs.length;
+        int col=strs[0].length();
+        int count=0;
+        for(int c=0;c<col;c++){
+            for(int r=1;r<row;r++){
+                if(strs[r].charAt(c)<strs[r-1].charAt(c)){
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
