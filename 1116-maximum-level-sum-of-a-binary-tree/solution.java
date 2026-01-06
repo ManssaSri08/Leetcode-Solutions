4470c3992c46b36b861531c87be7966b6753a0ed
ManssaSri08 <manssahari0827@gmail.com>
2026-01-06 15:24:00 +0000

    [LeetCode Sync] Runtime - 10 ms (16.01%), Memory - 49.2 MB (40.72%)

---
+++ 1116-maximum-level-sum-of-a-binary-tree/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxLevelSum(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int maxSum = Integer.MIN_VALUE;
+        int answer = 0;
+        int level = 0;
+
+        while (!q.isEmpty()) {
+            level++;
+            int size = q.size();
+            int sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                sum += node.val;
+                if (node.left != null) q.add(node.left);
+                if (node.right != null) q.add(node.right);
+            }
+            if (sum > maxSum) {
+                maxSum = sum;
+                answer = level;
+            }
+        }
+        return answer;
+    }
+}
