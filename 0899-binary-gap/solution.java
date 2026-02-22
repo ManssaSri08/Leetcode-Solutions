e8b93a0e5e54a1f58851cb9c204aef59e922d1dd
ManssaSri08 <manssahari0827@gmail.com>
2026-02-22 13:09:18 +0000

    [LeetCode Sync] Runtime - 1 ms (24.16%), Memory - 42.4 MB (12.95%)

---
+++ 0899-binary-gap/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int binaryGap(int n) {
+        String s=Integer.toBinaryString(n);
+        int prev=-1,ans=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='1'){
+                if(prev!=-1){
+                    ans=Math.max(ans,i-prev);
+                }
+                prev=i;
+            }
+        }
+        return ans;
+    }
+}
