fcf6fcf40a61b806ae6e64350f3b47ce21345559
ManssaSri08 <manssahari0827@gmail.com>
2026-03-03 11:47:51 +0000

    [LeetCode Sync] Runtime - 3 ms (43.11%), Memory - 42.4 MB (82.10%)

---
+++ 0728-self-dividing-numbers/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> list=new ArrayList<>();
+        for(int i=left;i<=right;i++){
+            int num=i;
+            int temp=i;
+            boolean valid=true;
+            while(temp>0){
+                int rem=temp%10;
+                if(rem==0 || num%rem!=0){
+                    valid=false; break;
+                }
+                temp/=10;
+            }
+            if(valid){
+                list.add(num);
+            }
+        }
+        return list;
+    }
+}
