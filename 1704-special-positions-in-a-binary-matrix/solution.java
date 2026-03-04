e82afc3b97218c15cea3e44c7dad2a697d319c53
ManssaSri08 <manssahari0827@gmail.com>
2026-03-04 12:18:03 +0000

    [LeetCode Sync] Runtime - 2 ms (96.57%), Memory - 47 MB (70.47%)

---
+++ 1704-special-positions-in-a-binary-matrix/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int row=mat.length;
+        int col=mat[0].length;
+        int count=0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(mat[i][j]==1){
+                    boolean special=true;
+                    for(int k=0;k<col;k++){
+                        if(k!=j && mat[i][k]==1){
+                            special=false;
+                            break;
+                        }
+                    }
+                    for(int k=0;k<row && special;k++){
+                        if(k!=i && mat[k][j]==1){
+                            special=false;
+                        }
+                    }
+                    if(special) count++;
+                }
+            }
+        }
+        return count;
+    }
+}
