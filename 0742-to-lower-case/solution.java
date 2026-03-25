9981f04ceb8f1404e7461bd7bfba36bf7c70484c
ManssaSri08 <manssahari0827@gmail.com>
2026-03-25 12:31:42 +0000

    [LeetCode Sync] Runtime - 1 ms (24.09%), Memory - 43 MB (51.96%)

--- 0742-to-lower-case/solution.java
+++ 0742-to-lower-case/solution.java
@@ -1,5 +1,11 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        StringBuilder sb=new StringBuilder();
+        for(char ch:s.toCharArray()){
+            if((ch>='a' && ch<='z')) sb.append(ch);
+            else if(!(Character.isLetter(ch))) sb.append(ch);
+            else sb.append((char)(ch+32));
+        }
+        return sb.toString();
     }
 }
