e5aa507d2c1fb616bb6348125cb5d8877e49fd87
ManssaSri08 <manssahari0827@gmail.com>
2026-04-16 05:16:33 +0000

    [LeetCode Sync] Runtime - 2 ms (98.01%), Memory - 43.8 MB (34.21%)

--- 1319-unique-number-of-occurrences/solution.java
+++ 1319-unique-number-of-occurrences/solution.java
@@ -1,10 +1,14 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int num:arr){
-            map.put(num,map.getOrDefault(num,0)+1);
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int n:arr){
+            map.put(n,map.getOrDefault(n,0)+1);
         }
-        HashSet<Integer> set=new HashSet<>(map.values());
-        return map.size()==set.size();
+        Set<Integer> set=new HashSet<>();
+        for(int freq:map.values()){
+            if(set.contains(freq)) return false;
+            set.add(freq);
+        }
+        return true;
     }
 }
