e7495ef645f9b569469c16aef88af07962253164
ManssaSri08 <manssahari0827@gmail.com>
2026-06-11 14:43:40 +0000

    [LeetCode Sync] Runtime - 16 ms (56.04%), Memory - 47.3 MB (38.93%)

---
+++ 0076-minimum-window-substring/solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char ch:t.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        int count=t.length(),minLen=Integer.MAX_VALUE,start=0,left=0;
+        for(int right=0;right<s.length();right++){
+            char ch=s.charAt(right);
+            if(map.containsKey(ch)){
+                if(map.get(ch)>0){
+                    count--;
+                }
+                map.put(ch,map.get(ch)-1);
+            }
+            while(count==0){
+                if(right-left+1<minLen){
+                    minLen=right-left+1;
+                    start=left;
+                }
+                char leftChar=s.charAt(left);
+                if(map.containsKey(leftChar)){
+                    map.put(leftChar,map.get(leftChar)+1);
+                    if(map.get(leftChar)>0){
+                        count++;
+                    }
+                }
+                left++;
+            }
+        }
+        return minLen==Integer.MAX_VALUE?"":s.substring(start,start+minLen);
+    }
+}
