a6bcf18509082012bc7902875b8178520ee35a71
ManssaSri08 <manssahari0827@gmail.com>
2026-06-15 13:17:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.1 MB (66.79%)

---
+++ 0670-maximum-swap/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maximumSwap(int num) {
+        char[] arr=String.valueOf(num).toCharArray();
+        int[] last=new int[10];
+        for(int i=0;i<arr.length;i++){
+            last[arr[i]-'0']=i;
+        }
+        for(int i=0;i<arr.length;i++){
+            int digit=arr[i]-'0';
+            for(int d=9;d>digit;d--){
+                if(last[d]>i){
+                    char temp=arr[i];
+                    arr[i]=arr[last[d]];
+                    arr[last[d]]=temp;
+                    return Integer.parseInt(new String(arr));
+                }
+            }
+        }
+        return num;
+    }
+}
