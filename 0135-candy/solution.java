be9943479efbf01cee30f7eb8e1ca615aca4afa3
ManssaSri08 <manssahari0827@gmail.com>
2026-07-10 13:08:23 +0000

    [LeetCode Sync] Runtime - 3 ms (86.31%), Memory - 47.8 MB (32.02%)

---
+++ 0135-candy/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length,ans=0;
+        int[] candies=new int[n];
+        Arrays.fill(candies,1);
+        for(int i=1;i<n;i++){
+            int rate=ratings[i];
+            if(rate>ratings[i-1]){
+                candies[i]=candies[i-1]+1;
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            int rate=ratings[i];
+            if(rate>ratings[i+1]){
+                candies[i]=Math.max(candies[i],candies[i+1]+1);
+            }
+        }
+        for(int c:candies) ans+=c;
+        return ans;
+    }
+}
