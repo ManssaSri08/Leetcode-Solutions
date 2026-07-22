3e9d4ce8eeb87e997a95fa78d772c022847565fa
ManssaSri08 <manssahari0827@gmail.com>
2026-07-22 12:07:43 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43 MB (23.30%)

--- 0213-house-robber-ii/solution.java
+++ 0213-house-robber-ii/solution.java
@@ -3,18 +3,16 @@ class Solution {
         int n=arr.length;
         if(n==1) return arr[0];
         if(n==2) return Math.max(arr[0],arr[1]);
-        int[] dp1=new int[n];
-        dp1[0]=arr[0];
-        dp1[1]=Math.max(arr[0],arr[1]);
-        for(int i=2;i<n-1;i++){
-            dp1[i]=Math.max(dp1[i-1],dp1[i-2]+arr[i]);
-        }
-        int[] dp2=new int[n];
-        dp2[1]=arr[1];
-        dp2[2]=Math.max(arr[1],arr[2]);
-        for(int i=3;i<n;i++){
-            dp2[i]=Math.max(dp2[i-1],dp2[i-2]+arr[i]);
-        }
-        return Math.max(dp1[n-2],dp2[n-1]);
+        int[] dp1=new int[n], dp2=new int[n];
+        Arrays.fill(dp1,-1); Arrays.fill(dp2,-1);
+        int case1=fun(n-2,0,arr,dp1), case2=fun(n-1,1,arr,dp2);
+        return Math.max(case1,case2);
+    }
+    public int fun(int i,int start,int[] arr,int[] dp){
+        if(i==start) return arr[start];
+        if(i<start) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int take=fun(i-2,start,arr,dp)+arr[i], notTake=fun(i-1,start,arr,dp);
+        return dp[i]=Math.max(take,notTake);
     }
 }
