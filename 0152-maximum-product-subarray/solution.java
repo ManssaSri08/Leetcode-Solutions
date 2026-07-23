34b9283141be415a3cf765dec7fac76825c44d3d
ManssaSri08 <manssahari0827@gmail.com>
2026-07-23 16:26:28 +0000

    [LeetCode Sync] Runtime - 3 ms (21.35%), Memory - 47.6 MB (15.86%)

--- 0152-maximum-product-subarray/solution.java
+++ 0152-maximum-product-subarray/solution.java
@@ -2,13 +2,9 @@ class Solution {
     public int maxProduct(int[] nums) {
         int max=nums[0],min=nums[0],ans=nums[0];
         for(int i=1;i<nums.length;i++){
-            if(nums[i]<0){
-                int temp=max;
-                max=min;
-                min=temp;
-            }
-            max=Math.max(nums[i],nums[i]*max);
-            min=Math.min(nums[i],nums[i]*min);
+            int tempMax=max;
+            max=Math.max(nums[i],Math.max(max*nums[i],min*nums[i]));
+            min=Math.min(nums[i],Math.min(tempMax*nums[i],min*nums[i]));
             ans=Math.max(max,ans);
         }
         return ans;
