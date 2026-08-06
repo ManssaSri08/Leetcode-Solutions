84c1a065df8609343df754231ae3a08f3be6af41
ManssaSri08 <manssahari0827@gmail.com>
2026-08-06 16:49:51 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.9 MB (75.05%)

--- 1833-find-the-highest-altitude/solution.java
+++ 1833-find-the-highest-altitude/solution.java
@@ -1,12 +1,10 @@
 class Solution {
     public int largestAltitude(int[] gain) {
         int n=gain.length,max=0;
-        int[] arr=new int[n+1];
-        arr[0]=0;
-        for(int i=0;i<n;i++){
-            arr[i+1]=arr[i]+gain[i];
+        for(int i=1;i<n;i++){
+            gain[i]+=gain[i-1];
         }
-        for(int a:arr){
+        for(int a:gain){
             if(a>max) max=a;
         }
         return max;
