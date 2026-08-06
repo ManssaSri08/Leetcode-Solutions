bd732f412c92bc9a11561ee1addf5f9a94f9030b
ManssaSri08 <manssahari0827@gmail.com>
2026-08-06 12:57:29 +0000

    [LeetCode Sync] Runtime - 1 ms (99.78%), Memory - 42.3 MB (94.00%)

---
+++ 3626-smallest-divisible-digit-product-i/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        for(int i=n;i<=n+t;i++){
+            int num=i,prod=1;
+            while(num>0){
+                prod*=(num%10);
+                num/=10;
+            }
+            if(prod%t==0) return i;
+        }
+        return n+t;
+    }
+}
