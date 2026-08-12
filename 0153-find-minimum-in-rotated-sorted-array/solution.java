3e8bf8594e3b8d5b10f48460ee058a7312cccf9c
ManssaSri08 <manssahari0827@gmail.com>
2026-08-12 15:57:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.6 MB (74.60%)

--- 0153-find-minimum-in-rotated-sorted-array/solution.java
+++ 0153-find-minimum-in-rotated-sorted-array/solution.java
@@ -1,19 +1,17 @@
 class Solution {
     public int findMin(int[] nums) {
-        if(nums.length==0 || nums==null) return 0;
-        if(nums.length==1) return nums[0];
-        int start=0,end=nums.length-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(mid>0 && nums[mid]<nums[mid-1])
-                return nums[mid];
-            if(nums[start]<=nums[mid] && nums[mid]>nums[end]){
-                start=mid+1;
+        int low=0,high=nums.length-1,ans=Integer.MAX_VALUE;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[low]<=nums[mid]){
+                ans=Math.min(ans,nums[low]);
+                low=mid+1;
             }
             else{
-                end=mid-1;
+                ans=Math.min(ans,nums[mid]);
+                high=mid-1;
             }
         }
-        return nums[start];
+        return ans;
     }
 }
