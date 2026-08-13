3d5f93f9af0ea0023e9be35d59f2c395603a1da7
ManssaSri08 <manssahari0827@gmail.com>
2026-08-13 14:12:40 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.1 MB (75.15%)

--- 0162-find-peak-element/solution.java
+++ 0162-find-peak-element/solution.java
@@ -1,15 +1,18 @@
 class Solution {
-    public int findPeakElement(int[] nums) {
-        int low=0,high=nums.length-1;
-        while(low<high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]>nums[mid+1]){
-                high=mid;
-            }
-            else{
+    public int findPeakElement(int[] arr) {
+        int n=arr.length,low=1,high=n-2;
+        if(n==1) return 0;
+        if(arr[0]>arr[1]) return 0;
+        if(arr[n-1]>arr[n-2]) return n-1;
+        while(low<=high){
+            int mid=(high+low)/2;
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]) 
+                return mid;
+            if(arr[mid]>arr[mid-1])
                 low=mid+1;
-            }
+            else
+                high=mid-1;
         }
-        return low;
+        return -1;
     }
 }
