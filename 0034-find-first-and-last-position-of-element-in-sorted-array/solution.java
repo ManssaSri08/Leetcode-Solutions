e9a44841f9cb84e965243d3198935d956e4a2d56
ManssaSri08 <manssahari0827@gmail.com>
2026-08-21 17:13:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 48.5 MB (14.20%)

--- 0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/solution.java
@@ -1,41 +1,35 @@
 class Solution {
+    public int lowerBound(int[] arr,int target){
+        int n=arr.length;
+        int low=0, high=n-1, ans=n;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>=target){
+                ans=mid; high=mid-1;
+            }
+            else
+                low=mid+1;
+        }
+        return ans;
+    }
+    public int upperBound(int[] arr,int target){
+        int n=arr.length;
+        int low=0, high=n-1, ans=n;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]>target){
+                ans=mid; high=mid-1;
+            }
+            else
+                low=mid+1;
+        }
+        return ans;
+    }
     public int[] searchRange(int[] nums, int target) {
-        int left=leftHalf(nums,target);
-        int right=rightHalf(nums,target);
-        return new int[]{left,right};
-    }
-    static int leftHalf(int[] nums,int target){
-        int start=0,end=nums.length-1,index=-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                index=mid;
-                end=mid-1;
-            }
-            else if(nums[mid]<target){
-                start=mid+1;
-            }
-            else{
-                end=mid-1;
-            }
-        }
-        return index;
-    }
-    static int rightHalf(int[] nums,int target){
-        int start=0,end=nums.length-1,index=-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                index=mid;
-                start=mid+1;
-            }
-            else if(nums[mid]<target){
-                start=mid+1;
-            }
-            else{
-                end=mid-1;
-            }
-        }
-        return index;
+        int first=lowerBound(nums,target);
+        if(first==nums.length || nums[first]!=target)
+            return new int[]{-1,-1};
+        int last=upperBound(nums,target)-1;
+        return new int[]{first,last};
     }
 }
